b07a2dcea1ef5521c19763134053f1c952021afa
CodeHart-art <jobhart11211@gmail.com>
2026-05-27 09:32:51 -0400

    UI.orderScreen full functionality

--- src/main/java/UI.java
+++ src/main/java/UI.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class UI {
     static Scanner userInput = new Scanner(System.in);
+    static String INPUT;
 
     public static void homeScreen() {
         while (true) {
@@ -10,9 +11,9 @@ public class UI {
                     1) NEW ORDER
                     0) EXIT
                     """);
-            String input = userInput.nextLine();
+            INPUT = userInput.nextLine();
 
-            switch (input) {
+            switch (INPUT) {
                 case "1":
                     orderScreen();
                     break;
@@ -20,7 +21,7 @@ public class UI {
                     System.out.println("EXITING PROGRAM...");
                     System.exit(0);
                 default:
-                    System.out.println("Select available options");
+                    System.out.println("Select from available options");
             }
         }
 
@@ -28,8 +29,52 @@ public class UI {
     }
 
     public static void orderScreen() {
-        System.out.println("Select");
+        boolean active = true;
+        while (active) {
+            System.out.println("Select your order");
+            System.out.println("""
+                    1) Add Sandwich
+                    2) Add Drink
+                    3) Add chips
+                    4) Checkout
+                    0) Cancel Order
+                    """);
+            INPUT = userInput.nextLine();
 
+            switch (INPUT) {
+                case "1":
+                    addSandwich();
+                    break;
+                case "2":
+                    addDrink();
+                    break;
+                case "3":
+                    addChips();
+                    break;
+                case "4":
+                    checkout();
+                    break;
+                case "0":
+                    active = false;
+                    System.out.println("Clearing order returning to main menu");
+                    break;
+                default:
+                    System.out.println("SELECT FROM AVAILABLE OPTIONS");
+                    break;
+            }
+        }
+    }
+
+    private static void checkout() {
+    }
+
+    private static void addChips() {
+    }
+
+    private static void addDrink() {
+    }
+
+    private static void addSandwich() {
     }
 
 
